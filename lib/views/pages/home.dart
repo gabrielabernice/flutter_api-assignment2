f779365fd110111aa1ba30339d64c26eaa265290
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-12-09 10:53:01 +0700

    fixing the initial code

--- lib/views/pages/home.dart
+++ lib/views/pages/home.dart
@@ -8,17 +8,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Province> provinceData = [];
+
+  List<Province> provinceData = []; ///
   bool isLoading = false;
 
-  // untuk mengambil data asynchronous
-  Future<dynamic> getProvinces() async {
+  Future<dynamic> getProvinces() async{////
     await MasterDataService.getProvince().then((value) {
       setState(() {
-        provinceData = value;
+        provinceData = value; ///
         isLoading = false;
       });
     });
+  }
+
+  dynamic cityDataOrigin;
+  dynamic cityIdOrigin;
+  dynamic selectedCityOrigin;
+
+  Future<List<City>> getCities(var provId) async{////
+    dynamic city;
+    await MasterDataService.getCity(provId).then((value) {
+      setState(() {
+        city = value;
+      });
+    });
+
+    return city;
   }
 
   @override
@@ -27,7 +42,8 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       isLoading = true;
     });
-    getProvinces();
+    getProvinces();////
+    cityDataOrigin = getCities("5");
   }
 
   @override
@@ -35,27 +51,82 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: const Text("Home Page"),
         centerTitle: true,
       ),
       body: Stack(
         children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: provinceData.isEmpty
-                ? const Align(
-                    alignment: Alignment.center,
-                    child: Text("Data tidak ditemukan"),
-                  )
-                : ListView.builder(
-                    itemCount: provinceData.length,
-                    itemBuilder: (context, index) {
-                      return CardProvince(provinceData[index]);
-                    },
+          Column(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Column(
+                    children: [
+                      Text("Origin"),
+                      Container(
+                        width: 240,
+                        child: FutureBuilder<List<City>>(
+                          future: cityDataOrigin,
+                          builder: (context, snapshot){
+                            if(snapshot.hasData){
+                              return DropdownButton(
+                                isExpanded: true,
+                                value: selectedCityOrigin,
+                                icon: Icon(Icons.arrow_drop_down),
+                                iconSize: 30,
+                                elevation: 4,
+                                style: TextStyle(color: Colors.black),
+                                hint: selectedCityOrigin == null
+                                ? Text('Pilih kota')
+                                : Text(selectedCityOrigin.cityName),
+                                items: snapshot.data!.map<DropdownMenuItem<City>>((City value) {
+                                  return DropdownMenuItem(
+                                    value: value,
+                                    child: Text(value.cityName.toString())
+                                  );
+                                }).toList(),
+                                onChanged: (newValue){
+                                  setState(() {
+                                    selectedCityOrigin = newValue;
+                                    cityIdOrigin = selectedCityOrigin.cityId;
+                                  });
+                                }
+                              );
+                            }else if(snapshot.hasError){
+                              return Text("Tidak ada data");
+                            }
+                            return UiLoading.loadingSmall();
+                          }),
+                      )
+                    ],
                   ),
+                ),
+              ),
+              Flexible(
+                flex: 5,
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: provinceData.isEmpty
+                  ? const Align(
+                      alignment: Alignment.center,
+                      child: Text("Data tidak ditemukan"),
+                  )
+                  : ListView.builder(
+                    itemCount: provinceData.length,
+                    itemBuilder: (context, index){
+                      return CardProvince(provinceData[index]);
+                    }
+                  )
+                ),
+              ),
+            ],
           ),
-          isLoading == true ? UiLoading.loadingBlock() : Container()
+          isLoading == true ? UiLoading.loadingBlock()
+          : Container()
         ],
       ),
     );
